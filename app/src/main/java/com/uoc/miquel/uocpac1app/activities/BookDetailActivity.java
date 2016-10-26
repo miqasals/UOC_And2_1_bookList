4e0c379da6842cd34da6c0c08460a1a89c8e1007
Miquel <miqasals@gmail.com>
2016-10-26 22:29:37 +0200

    PAC2.EX2 finalitzat
     - Creat el projecte en Firebase.
     - Importada llista de llibres.json.
     - Integrat Firebase a l'aplicació.
    
     - Activació de les llibreries de autenticació i base de dades.
     - Creació d'un usuari "miqasals@gmail.com" amb pass "123456"
     - Implementació del codi per autenticar-se a la base de dades des de l'aplicació.

--- app/src/main/java/com/uoc/miquel/uocpac1app/activities/BookDetailActivity.java
+++ app/src/main/java/com/uoc/miquel/uocpac1app/activities/BookDetailActivity.java
@@ -27,46 +27,24 @@ public class BookDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
 
-        //Creamos el objeto toolbar para poder cambiar el titulo al nombre del book.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_book_detail);
         ActionBar actionBar = getSupportActionBar();
-        //Activamos el boton de vuelta atras a la activity Book List.
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        //Se crea instancia de la clase que gestiona el fragment.
         BookDetailFragment bookDetailFrag = new BookDetailFragment();
-
-        //Creamos un Bundle para poder enviar datos (posicion del book elegido) al fragment.
         Bundle posArg = new Bundle();
         int pos = getIntent().getIntExtra("position",0);
         posArg.putInt("position",pos);
-
-        /*  Exercici 1 (primera versió de layout)
-        //Es modifica el titol del llibre
-        TextView titol = (TextView) findViewById(R.id.book_detail_titol);
-        titol.setText(BookContent.ITEMS.get(posArg.getInt("position")).getTitol());
-        */
-
-        //Se modifica el titulo para que muestre el titulo del libro. Se hace aqui porque necesitamos
-        //haver recibido de BookListActivity la posicion a través del Intent.
         toolbar.setTitle(BookContent.ITEMS.get(posArg.getInt("position")).getTitol());
-
-        //Se pasa el bundle al fragment.
         bookDetailFrag.setArguments(posArg);
-
-        //Enlazamos el layout y la instancia del fragment para que se inicie y se muestre por pantalla.
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.frag_book_detail, bookDetailFrag)
                 .commit();
-
-
-
     }
 
-    //Configuramos las acciones de la actionBar. En este caso solo hay la opcion de volver atras.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
