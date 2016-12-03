7a2e89c9b8041c3e1de9221d41715dac6f58b34e
Miquel Casals <miqasals@gmail.com>
2016-12-03 23:47:13 +0100

    PAC3. EX2. Visibles les accions de la notificació. Pendent implemtentar l'acció a realitzar i les icones correctes.

--- app/src/main/java/com/uoc/miquel/uocpac1app/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/uoc/miquel/uocpac1app/services/MyFirebaseMessagingService.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -30,28 +32,30 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         //Creem una notificació local a partir de les dades rebudes de la notificació Firebase.
-        sendNotification(remoteMessage.getNotification().getBody(),0);    //S'ha d'obtenir el valor de la clau valor.
+        Log.i("MYFIREBASE","onMessageReceived");
+        sendNotification(remoteMessage.getNotification().getBody(),0);
     }
 
     private void sendNotification(String messageBody, int position) {
 
-        BookContent.BookItem bookItem;
+        //BookContent.BookItem bookItem;
 
         //obtenim el book a partir de la posicio
-        bookItem = BookContent.getBooks().get(position);
+        //bookItem = BookContent.getBooks().get(position);
 
+
+        ///////////////// INTENTS  /////////////////////
         // Creem un intent per quan vulguem veure el detall del llibre
         Intent detailIntent = new Intent(this, BookDetailActivity.class);
-        //TODO: Que fan els FLAGS????
-        detailIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        detailIntent.setAction(ACTION_DETAIL);
+        PendingIntent piDetail = PendingIntent.getActivity(this,(int) System.currentTimeMillis(),detailIntent,0);
+
 
         //Creem un intent per quan vulguem borrar el llibre de la based dades local.
-        Intent listIntent = new Intent(this, BookDetailActivity.class);
-        listIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        //TODO: Que es el PendingIntent????
-        PendingIntent piDetail = PendingIntent.getActivity(this,0,detailIntent,PendingIntent.FLAG_ONE_SHOT);
-        PendingIntent piErase = PendingIntent.getActivity(this,0,listIntent,PendingIntent.FLAG_ONE_SHOT);
+        Intent eraseIntent = new Intent(this, BookDetailActivity.class);
+        eraseIntent.setAction(ACTION_ERASE);
+        PendingIntent piErase = PendingIntent.getActivity(this,(int) System.currentTimeMillis(),eraseIntent,0);
+        ///////////////////////////////////////////////
 
         //Es crea un so per la notificacio
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -61,26 +65,50 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 //Configurem la icona de la barra d'estat
                 .setSmallIcon(R.mipmap.ic_launcher)
                 //Titol de la notificacio visible quan esta extesa.
-                .setContentTitle(bookItem.getTitle())
+                .setContentTitle("Firebase")
                 //Text de la notificacio visible quan esta extesa.
-                //.setContentText(messageBody)
-                //TODO: Que fa setAutoCancel????
+                .setContentText("Text normal de la notificació")
+                //Indiquem a la notificació que es pot tancar despres de presionar-la
                 .setAutoCancel(true)
                 //Asigna el so que hem creat anteriorment.
                 .setSound(defaultSoundUri)
 
                 .setStyle(new NotificationCompat.BigTextStyle()
-                    .bigText(messageBody))
-                .addAction(R.mipmap.ic_launcher,"Eliminar",piErase)
-                .addAction(R.mipmap.ic_launcher,"Veure",piDetail);
-                //TODO: Que fa el setContentIntent????
-                //.setContentIntent(pendingIntent);
+                    .bigText("Text extra de la notificació expandida."))
+                .addAction(new NotificationCompat.Action(R.mipmap.ic_launcher,"Eliminar",piErase))
+                .addAction(new NotificationCompat.Action(R.mipmap.ic_launcher,"Veure",piDetail));
 
         //Es crea una instancia del gestor de notificacions.
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         //S'executa la notificació.
-        notificationManager.notify(0,notificationBuilder.build());
+        notificationManager.notify(0, notificationBuilder.build());
     }
+
+
+
+    private void sendNotification2 (String msg, int i) {
+
+        Intent intent = new Intent(this, BookListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(msg)
+                .setContentTitle("Titol de la notificacio")
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        nm.notify(0, notificationBuilder.build());
+
+    }
+
+
 
 }
